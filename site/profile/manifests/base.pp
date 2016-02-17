d2ab6397fb9f1ae531a6916fbb50f516c1ea9ed0
kreczko <lkreczko@googlemail.com>
2016-02-17 15:32:18 +0000

    added feature to remove packages

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,4 +1,8 @@
 # the base profile should include component modules that will be on all nodes
 class profile::base {
   class { '::ntp': }
+
+  $packages_to_remove = hiera_array('packages_to_remove', [])
+
+  package { $packages_to_remove: ensure => 'absent', }
 }
