56c93ed7f773b104ec7aab9dd29095b18c816cd6
Luke Kreczko <lkreczko@googlemail.com>
2016-02-19 14:15:56 +0000

    removing cert extension based roles

--- site.pp
+++ site.pp
@@ -36,9 +36,4 @@ node default {
   #incude a role on any node that specifies it's role via a trusted fact at provision time
   #https://docs.puppetlabs.com/puppet/latest/reference/lang_facts_and_builtin_vars.html#trusted-facts
   #https://docs.puppetlabs.com/puppet/latest/reference/ssl_attributes_extensions.html#aws-attributes-and-extensions-population-example
-
-  if !empty( $trusted['extensions']['pp_role'] ) {
-    include "role::${trusted['extensions']['pp_role']}"
-  }
-
 }
