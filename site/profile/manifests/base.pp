6e696c77e6d53a46802345dc3122a11fcf10b6db
Luke Kreczko <lkreczko@googlemail.com>
2016-02-19 16:58:38 +0000

    added packages_to_install

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,7 +2,9 @@
 class profile::base {
   class { '::ntp': }
 
+  $packages_to_install = hiera_array('packages_to_install', [])
   $packages_to_remove = hiera_array('packages_to_remove', [])
 
+  package { $packages_to_install: ensure => 'present', }
   package { $packages_to_remove: ensure => 'absent', }
 }
