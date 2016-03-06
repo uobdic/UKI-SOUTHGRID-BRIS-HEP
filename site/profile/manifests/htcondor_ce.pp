203cc0f0858742691fd6d92520b1ad6c29dcf9e6
Lukasz Kreczko <lkreczko@googlemail.com>
2016-03-06 11:39:52 +0000

    added goc_site_name

--- site/profile/manifests/htcondor_ce.pp
+++ site/profile/manifests/htcondor_ce.pp
@@ -1,9 +1,10 @@
 class profile::htcondor_ce {
-  $uid_domain     = hiera('htcondor::uid_domain', $::domain)
-  $managers       = hiera('htcondor::managers', [])
-  $argus_server   = $::site_info['argus_server']
-  $argus_port     = $::site_info['argus_port']
-  $supported_vos  = $::site_info['supported_vos']
+  $uid_domain    = hiera('htcondor::uid_domain', $::domain)
+  $managers      = hiera('htcondor::managers', [])
+  $argus_server  = $::site_info['argus_server']
+  $argus_port    = $::site_info['argus_port']
+  $supported_vos = $::site_info['supported_vos']
+  $goc_site_name = $::site_info['gocdb_name']
 
   class { '::htcondor_ce':
     pool_collectors => $managers,
@@ -11,5 +12,6 @@ class profile::htcondor_ce {
     argus_server    => $argus_server,
     argus_port      => $argus_port,
     supported_vos   => $supported_vos,
+    goc_site_name   => $goc_site_name,
   }
 }
