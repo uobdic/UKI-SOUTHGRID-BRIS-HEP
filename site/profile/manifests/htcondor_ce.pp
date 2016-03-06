62d49071f29bb01c5190ba5725a63431e1bfbbc2
Lukasz Kreczko <lkreczko@googlemail.com>
2016-03-06 11:07:55 +0000

    updated HTCondor CE settings

--- site/profile/manifests/htcondor_ce.pp
+++ site/profile/manifests/htcondor_ce.pp
@@ -2,14 +2,15 @@ class profile::htcondor_ce {
   $uid_domain     = hiera('htcondor::uid_domain', $::domain)
   $managers       = hiera('htcondor::managers', [])
   $pool_collector = join($managers, ',')
-  $condor_version = $::condor_version
+  $argus_server   = $::site_info['argus_server']
+  $argus_port     = $::site_info['argus_port']
+  $supported_vos  = $::site_info['supported_vos']
 
   class { '::htcondor_ce':
     pool_collector => $pool_collector,
     uid_domain     => $uid_domain,
-    lrms_version   => $condor_version,
-  }
-
-  class { '::htcondor_ce::bdii':
+    argus_server   => $argus_server,
+    argus_port     => $argus_port,
+    supported_vos  => $supported_vos,
   }
 }
