dd01cf8fa2b7135215f59cf443e60c34deabbe2d
Lukasz Kreczko <lkreczko@googlemail.com>
2016-03-06 11:24:36 +0000

    added jobroutes and changed collector entries

--- site/profile/manifests/htcondor_ce.pp
+++ site/profile/manifests/htcondor_ce.pp
@@ -1,16 +1,15 @@
 class profile::htcondor_ce {
   $uid_domain     = hiera('htcondor::uid_domain', $::domain)
   $managers       = hiera('htcondor::managers', [])
-  $pool_collector = join($managers, ',')
   $argus_server   = $::site_info['argus_server']
   $argus_port     = $::site_info['argus_port']
   $supported_vos  = $::site_info['supported_vos']
 
   class { '::htcondor_ce':
-    pool_collector => $pool_collector,
-    uid_domain     => $uid_domain,
-    argus_server   => $argus_server,
-    argus_port     => $argus_port,
-    supported_vos  => $supported_vos,
+    pool_collectors => $managers,
+    uid_domain      => $uid_domain,
+    argus_server    => $argus_server,
+    argus_port      => $argus_port,
+    supported_vos   => $supported_vos,
   }
 }
