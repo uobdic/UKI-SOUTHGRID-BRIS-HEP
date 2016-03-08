84f04c3fe258f6a3a631aefa819c3ffcacea9490
kreczko <lkreczko@googlemail.com>
2016-03-08 17:27:29 +0000

    added htcondor worker role

---
+++ site/role/manifests/htcondor_worker.pp
@@ -0,0 +1,26 @@
+class role::htcondor_worker {
+  $custom_attributes     = hiera_hash('htcondor::custom_attributes', {
+  }
+  )
+  $custom_job_attributes = hiera_hash('htcondor::custom_job_attributes', {
+  }
+  )
+
+  $site_attributes       = {
+    'CLUSTER' => $::node_info['cluster'],
+  }
+
+  $site_job_attributes   = {
+    'HEPSPEC06'               => $::node_info['hepspec06'],
+    'ACCOUNTING_SCALE_FACTOR' => $::node_info['accounting_scale_factor'],
+  }
+
+  $merged_attributes     = merge($custom_attributes, $site_attributes)
+  $merged_job_attributes = merge($custom_job_attributes, $site_job_attributes)
+
+  class { '::htcondor':
+    custom_attributes     => $custom_attributes,
+    custom_job_attributes => $merged_job_attributes,
+  }
+
+}
