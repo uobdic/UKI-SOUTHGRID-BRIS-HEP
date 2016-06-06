84215a63245f0a51dc5d9d7376995475aea4e455
kreczko <lkreczko@googlemail.com>
2016-06-06 12:15:11 +0100

    updated condor parameters (renaming)

--- site/role/manifests/htcondor_worker.pp
+++ site/role/manifests/htcondor_worker.pp
@@ -1,28 +1,32 @@
 class role::htcondor_worker {
-  $custom_attributes     = hiera_hash('htcondor::custom_attributes', {
+  $custom_machine_attributes = hiera_hash('htcondor::custom_machine_attributes',
+  {
   }
   )
-  $custom_job_attributes = hiera_hash('htcondor::custom_job_attributes', {
+  $custom_job_attributes     = hiera_hash('htcondor::custom_job_attributes', {
   }
   )
 
-  $site_attributes       = {
+  $site_machine_attributes   = {
     'CLUSTER'                 => $::node_info['cluster'],
     'HEPSPEC06'               => $::node_info['hepspec06'],
     'ACCOUNTING_SCALE_FACTOR' => $::node_info['accounting_scale_factor'],
   }
 
-  $site_job_attributes   = {
+  $site_job_attributes       = {
     'HEPSPEC06'               => $::node_info['hepspec06'],
     'ACCOUNTING_SCALE_FACTOR' => $::node_info['accounting_scale_factor'],
   }
 
-  $merged_attributes     = merge($custom_attributes, $site_attributes)
-  $merged_job_attributes = merge($custom_job_attributes, $site_job_attributes)
+  $merged_machine_attributes = merge($custom_machine_attributes,
+  $site_machine_attributes)
+
+  $merged_job_attributes     = merge($custom_job_attributes,
+  $site_job_attributes)
 
   class { '::htcondor':
-    custom_attributes     => $merged_attributes,
-    custom_job_attributes => $merged_job_attributes,
+    custom_machine_attributes => $merged_machine_attributes,
+    custom_job_attributes     => $merged_job_attributes,
   }
 
 }
