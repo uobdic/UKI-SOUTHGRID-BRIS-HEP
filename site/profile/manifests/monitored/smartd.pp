57e78d9193786f75d14e781abbe523486e297cc3
kreczko <lkreczko@googlemail.com>
2016-06-07 11:32:48 +0100

    added smartmontools package installation

--- site/profile/manifests/monitored/smartd.pp
+++ site/profile/manifests/monitored/smartd.pp
@@ -1,15 +1,19 @@
 class profile::monitored::smartd {
   if !$::is_virtual {
+    package { 'smartmontools': ensure => 'installed', }
+
     file { '/etc/smartd.conf':
       ensure  => 'present',
       content => template("${module_name}/smartd.conf.erb"),
       mode    => '0644',
       notify  => Service['smartd'],
+      require => [Package['smartmontools'],]
     }
 
     service { 'smartd':
-      ensure => 'running',
-      enable => true,
+      ensure  => 'running',
+      enable  => true,
+      require => [Package['smartmontools'],]
     }
   }
 }
