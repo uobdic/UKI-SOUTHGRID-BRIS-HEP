0207b3b4b4664afdb0954d80f7a55e2e06d687f6
kreczko <lkreczko@googlemail.com>
2016-06-07 09:33:58 +0100

    central servers should not be logging twice to themselves

--- site/profile/manifests/monitored/central_log.pp
+++ site/profile/manifests/monitored/central_log.pp
@@ -1,11 +1,16 @@
 class profile::monitored::central_log {
-  $central_log = hiera_array('profile::monitored::central_log', [])
+  $central_log    = hiera_array('profile::monitored::central_log', [])
 
-  file { '/etc/rsyslog.d/central.conf':
-    ensure  => 'present',
-    content => template("${module_name}/central.conf.erb"),
-    mode    => '0644',
-    notify  => Service['rsyslog'],
+  $is_central_log = member($central_log, $::fqdn) or member($central_log,
+  $::ipaddress)
+
+  unless $is_central_log {
+    file { '/etc/rsyslog.d/central.conf':
+      ensure  => 'present',
+      content => template("${module_name}/central.conf.erb"),
+      mode    => '0644',
+      notify  => Service['rsyslog'],
+    }
   }
 
   service { 'rsyslog':
