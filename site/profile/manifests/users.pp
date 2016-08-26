4d974fdc4a4e3a9c230cdcc579c1b57dd95336d1
kreczko <lkreczko@googlemail.com>
2016-08-26 13:51:21 +0100

    attempt to add users to new soolin

--- site/profile/manifests/users.pp
+++ site/profile/manifests/users.pp
@@ -1,25 +1,23 @@
 class profile::users {
-  $groups       = hiera_hash('profile::users::groups', {
+  $empty_hash  = {
   }
-  )
-  $users        = hiera_hash('profile::users::users', {
-  }
-  )
-  if empty($groups){
+  $groups      = hiera_hash('profile::users::groups', $empty_hash)
+  $users       = hiera_hash('profile::users::users', $empty_hash)
+
+  if empty($groups) {
     notice('No profile::users::groups specified')
   }
-  if empty($users){
+
+  if empty($users) {
     notice('No profile::users::users specified')
   }
   $node_info = hiera_hash('site::node_info')
 
-  if ($node_info['role'] == 'hdfs_namenode'){
-    $shell        = hiera('profile::users::shell', '/sbin/nologin')
+  if ($node_info['role'] == 'hdfs_namenode') {
+    $shell = hiera('profile::users::shell', '/sbin/nologin')
+  } else {
+    $shell = hiera('profile::users::shell', '/bin/bash')
   }
-  else {
-    $shell        = hiera('profile::users::shell', '/bin/bash')
-  }
-
 
   $defaults     = {
     'ensure' => present,
@@ -36,4 +34,18 @@ class profile::users {
     create_resources('group', $groups, $defaults)
     create_resources('account', $users, $acc_defaults)
   }
+
+  if $::fqdn == 'soolin.dice.priv' {
+    create_resources('group', $groups, $defaults)
+    $soolin_defaults = {
+      'ensure'       => present,
+      'shell'        => $shell,
+      'password'     => '!!',
+      'create_group' => false,
+      'managehome'   => false, # do not create home folders
+    }
+    # use puppetlabs/accounts
+    create_resources('accounts', $users, $soolin_defaults)
+  }
+
 }
