8aa596552404b17c18d89c424fd7cd93b02d62ee
kreczko <lkreczko@googlemail.com>
2016-11-25 08:27:58 +0000

    added bristol htcondor extras

---
+++ site/profile/manifests/htcondor_scheduler.pp
@@ -0,0 +1,12 @@
+class profile::htcondor_scheduler {
+  $condor_user = hiera('htcondor::condor_user')
+  $condor_group = hiera('htcondor::condor_group')
+  file{'/etc/condor/config.d/99_bristol_extras.conf':
+    source => "puppet:///modules/${module_name}/99_htcondor_extras.conf",
+    require => Package['condor'],
+    owner   => $condor_user,
+    group   => $condor_group,
+    mode    => '0644',
+    notify  => Exec['/usr/sbin/condor_reconfig'],
+  }
+}
