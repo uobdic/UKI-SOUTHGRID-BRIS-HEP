0fe186baf8ba6974f5095f1e0521a547d741f63c
kreczko <lkreczko@googlemail.com>
2016-11-25 08:43:56 +0000

    added custom configs for smartd

--- site/profile/manifests/monitored/smartd.pp
+++ site/profile/manifests/monitored/smartd.pp
@@ -2,12 +2,25 @@ class profile::monitored::smartd {
   if !$::is_virtual {
     package { 'smartmontools': ensure => 'installed', }
 
-    file { '/etc/smartd.conf':
-      ensure  => 'present',
-      content => template("${module_name}/smartd.conf.erb"),
-      mode    => '0644',
-      notify  => Service['smartd'],
-      require => [Package['smartmontools'],]
+    $custom_config_machines = ['soolin.dice.priv']
+
+    if member($custom_config_machines, $::fqdn){
+      file { '/etc/smartd.conf':
+        ensure  => 'present',
+        source => "puppet:///modules/${module_name}/smart.d/${fqdn}.conf",
+        mode    => '0644',
+        notify  => Service['smartd'],
+        require => [Package['smartmontools'],]
+      }
+    }
+    else {
+      file { '/etc/smartd.conf':
+        ensure  => 'present',
+        content => template("${module_name}/smartd.conf.erb"),
+        mode    => '0644',
+        notify  => Service['smartd'],
+        require => [Package['smartmontools'],]
+      }
     }
 
     service { 'smartd':
