a907926ea3c06cf06c6edbbd60a29b8556d000d4
kreczko <lkreczko@googlemail.com>
2017-01-24 10:19:10 +0000

    added ipv6 syntax

--- site/profile/manifests/firewall.pp
+++ site/profile/manifests/firewall.pp
@@ -1,17 +1,33 @@
+#
+#  Firewall profile
+#
 class profile::firewall {
   $accept          = hiera_hash('profile::firewall::accept', {})
+  $accept_v6       = hiera_hash('profile::firewall6::accept', {})
   $drop            = hiera_hash('profile::firewall::drop', {})
+  $drop_v6         = hiera_hash('profile::firewall6::drop', {})
   include profile::firewall::setup
 
   $accept_defaults = {
     'action' => 'accept',
   }
+  $accept_defaults_v6 = {
+    'action' => 'accept',
+    'provider' => 'ip6tables'
+  }
 
   $drop_defaults   = {
     'action' => 'drop',
   }
+  $drop_defaults_v6   = {
+    'action' => 'drop',
+    'provider' => 'ip6tables'
+  }
 
   create_resources('firewall', $accept, $accept_defaults)
   create_resources('firewall', $drop, $drop_defaults)
+
+  create_resources('firewall', $accept_v6, $accept_defaults_v6)
+  create_resources('firewall', $drop_v6, $drop_defaults_v6)
 
 }
