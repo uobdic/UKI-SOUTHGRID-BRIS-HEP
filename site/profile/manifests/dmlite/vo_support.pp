ddd01ffeb2424abaf2f39302477a4e5d47a7a405
Luke Kreczko <lkreczko@googlemail.com>
2017-12-13 12:58:05 +0000

    Local changes (#4)
    * added empty mount_under_scratch_dirs
    
    * fixed & added pool accounts for glexec
    
    * increased proxy delegation time from 24h to 72h
    
    * added voms servers for new VOs

--- site/profile/manifests/dmlite/vo_support.pp
+++ site/profile/manifests/dmlite/vo_support.pp
@@ -2,7 +2,7 @@ class profile::dmlite::vo_support {
   $supported_vos = $::site_info['supported_vos']
 
   # for the moment use this safe list instead of $supported_vos
-  $vo_array      = ['atlas', 'cms', 'dteam', 'ops', 'lhcb', 'alice']
+  $vo_array      = ['atlas', 'cms', 'dteam', 'gridpp', 'ops', 'lhcb', 'alice', 'lsst', 'vo_southgrid_ac_uk',]
   $vos           = prefix($vo_array, 'voms::')
 
   class { $vos: }
@@ -21,7 +21,16 @@ class profile::dmlite::vo_support {
     'vomss://lcg-voms2.cern.ch:8443/voms/alice?/alice'  => 'alice',
     'vomss://voms2.cern.ch:8443/voms/ops?/ops'          => 'ops',
     'vomss://lcg-voms2.cern.ch:8443/voms/ops?/ops'      => 'ops',
-    'vomss://voms.hellasgrid.gr:8443/voms/dteam?/dteam' => 'dteam'
+    'vomss://voms.hellasgrid.gr:8443/voms/dteam?/dteam' => 'dteam',
+    'vomss://voms.gridpp.ac.uk:8443/voms/gridpp?/gridpp' => 'gridpp',
+    'vomss://voms02.gridpp.ac.uk:8443/voms/gridpp?/gridpp' => 'gridpp',
+    'vomss://voms03.gridpp.ac.uk:8443/voms/gridpp?/gridpp' => 'gridpp',
+    'vomss://voms.fnal.gov:8443/voms/lsst?/lsst'           => 'lsst',
+    'vomss://voms1.fnal.gov:8443/voms/lsst?/lsst'          => 'lsst',
+    'vomss://voms2.fnal.gov:8443/voms/lsst?/lsst'          => 'lsst',
+    'vomss://voms.gridpp.ac.uk:8443/voms/vo.southgrid.ac.uk?/vo.southgrid.ac.uk'   => 'vo.southgrid.ac.uk',
+    'vomss://voms02.gridpp.ac.uk:8443/voms/vo.southgrid.ac.uk?/vo.southgrid.ac.uk' => 'vo.southgrid.ac.uk',
+    'vomss://voms03.gridpp.ac.uk:8443/voms/vo.southgrid.ac.uk?/vo.southgrid.ac.uk' => 'vo.southgrid.ac.uk',
   }
 
   lcgdm::mkgridmap::file { 'lcgdm-mkgridmap':
