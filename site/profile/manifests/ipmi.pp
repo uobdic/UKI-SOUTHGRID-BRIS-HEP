990c31421250a71c266fd738c1d2d95fc6bbebbe
kreczko <lkreczko@googlemail.com>
2019-09-05 11:59:36 +0100

    added VLAN setting to IPMI profile

--- site/profile/manifests/ipmi.pp
+++ site/profile/manifests/ipmi.pp
@@ -4,14 +4,15 @@ class profile::ipmi() {
 
   $network_ip = hiera('profile::ipmi::network_ip', '0.0.0.0')
 
-  ipmi::network { 'lan1':
-  type    => 'static',
-  ip      => $network_ip,
-  netmask => '255.255.255.0',
-  gateway => '192.168.1.1',
-  }->exec{'Set VLAN':
-    command => 'ipmitool lan print 1',
-    path    => ['/usr/bin', '/usr/sbin',],
+  unless $network_ip == '0.0.0.0'{
+    ipmi::network { 'lan1':
+      type    => 'static',
+      ip      => $network_ip,
+      netmask => '255.255.255.0',
+      gateway => '10.129.13.250',
+    }->exec{'set VLAN':
+      command => 'ipmitool lan set 1 vlan id 715',
+      path    => ['/usr/bin', '/usr/sbin',],
+    }
   }
-
 }
