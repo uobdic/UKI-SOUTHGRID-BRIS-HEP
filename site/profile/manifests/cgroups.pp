7d8587c26182a4d874cf50cd2204039bc0d8051a
kreczko <lkreczko@googlemail.com>
2019-12-05 17:36:10 +0000

    fixing cgroups for CentOS 7

--- site/profile/manifests/cgroups.pp
+++ site/profile/manifests/cgroups.pp
@@ -1,17 +1,22 @@
+# Configuration for cgroups
+# see https://www.gridpp.ac.uk/wiki/Enable_Cgroups_in_HTCondor
 class profile::cgroups {
   package { 'libcgroup': ensure => installed, }
 
-  service { 'cgconfig':
-    ensure  => running,
-    enable  => true,
-    require => [Package['libcgroup']],
-  }
+  if $::osfamily == 'Redhat' and $::operatingsystemmajrelease == '6' {
+    # in CentOS7 cgroups is automatically setup and all the systemd services are enabled
+    service { 'cgconfig':
+      ensure  => running,
+      enable  => true,
+      require => [Package['libcgroup']],
+    }
 
-  file { '/etc/cgconfig.d/00_cg_htcondor.conf':
-    ensure  => present,
-    source  => "puppet:///modules/${module_name}/00_cg_htcondor.conf",
-    require => [Package['libcgroup']],
-    notify  => Service['cgconfig'],
+    file { '/etc/cgconfig.d/00_cg_htcondor.conf':
+      ensure  => present,
+      source  => "puppet:///modules/${module_name}/00_cg_htcondor.conf",
+      require => [Package['libcgroup']],
+      notify  => Service['cgconfig'],
+    }
   }
 
 }
