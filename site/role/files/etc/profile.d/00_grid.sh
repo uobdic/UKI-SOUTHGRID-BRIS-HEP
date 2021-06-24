70bb05005dba0f8acbf29527d0d3848fad5db360
kreczko <lkreczko@googlemail.com>
2021-06-24 15:10:43 +0100

    fixed spacing for 00_grid.sh

--- site/role/files/etc/profile.d/00_grid.sh
+++ site/role/files/etc/profile.d/00_grid.sh
@@ -2,10 +2,11 @@
 
 export PATH=/cvmfs/oasis.opensciencegrid.org/mis/singularity/bin:$PATH
 
-if [ "x$USER" != "xroot" ]; then
-        source /cvmfs/grid.cern.ch/umd-c7wn-latest/etc/profile.d/setup-c7-wn-example.sh
-        # to fix java for the hadoop commands:
-        unset JAVA_HOME
+if [ "x$USER" != "xroot" ]
+then
+  source /cvmfs/grid.cern.ch/umd-c7wn-latest/etc/profile.d/setup-c7-wn-example.sh
+  # to fix java for the hadoop commands:
+  unset JAVA_HOME
 else
-        echo "User root detected - not loading grid setup"
+  echo "User root detected - not loading grid setup"
 fi
