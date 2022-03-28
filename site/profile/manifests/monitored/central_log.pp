58d288d16d87af25b3cb14cf8e0279b72da009da
Luke Kreczko <lkreczko@googlemail.com>
2022-03-28 10:41:36 +0100

    simplified rsyslog configs

--- site/profile/manifests/monitored/central_log.pp
+++ site/profile/manifests/monitored/central_log.pp
@@ -6,6 +6,51 @@ class profile::monitored::central_log {
   $::ipaddress)
 
   unless $is_central_log {
-    include rsyslog::config
+    # include rsyslog::config
+    # remove obsolete config
+    file { '/etc/rsyslog.d/central.conf':
+      ensure => 'absent',
+    }
+
+    # local logging
+    file { '/etc/rsyslog.d/10_local.conf':
+      ensure => 'present',
+      source => "puppet:///modules/${module_name}/etc/rsyslog.d/10_local.conf",
+      mode   => '0644',
+      notify => Service['rsyslog'],
+    }
+
+    # remote DICE logging
+    unless empty($central_log)
+    {
+      file { '/etc/rsyslog.d/20_dice.conf':
+        ensure  => 'present',
+        content => template("${module_name}/etc/rsyslog.d/20_dice.conf.erb"),
+        mode    => '0644',
+        notify  => Service['rsyslog'],
+      }
+    }
+
+    # remote IT logging
+    unless empty($it_services_log) {
+      file { '/etc/rsyslog.d/99_it_services.conf':
+        ensure  => 'present',
+        content => template("${module_name}/etc/rsyslog.d/99_it_services.conf.erb"),
+        mode    => '0644',
+        notify  => Service['rsyslog'],
+      }
+    }
+
+    file { '/etc/rsyslog.conf':
+      ensure => 'present',
+      source => "puppet:///modules/${module_name}/etc/rsyslog.conf",
+      mode   => '0644',
+      notify => Service['rsyslog'],
+    }
+  }
+
+  service { 'rsyslog':
+    ensure => 'running',
+    enable => true,
   }
 }
