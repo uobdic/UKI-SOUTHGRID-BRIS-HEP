cd39cb09d95597aea0049167dc98830ee4d6c03c
Luke Kreczko <lkreczko@googlemail.com>
2022-08-24 12:25:20 +0100

    smartd: exclude VM hosts

--- site/profile/manifests/monitored/smartd.pp
+++ site/profile/manifests/monitored/smartd.pp
@@ -1,32 +1,30 @@
 class profile::monitored::smartd {
-  if !$::is_virtual {
+  if !$fact::is_virtual {
     package { 'smartmontools': ensure => 'installed', }
 
-    $custom_config_machines = ['soolin.dice.priv']
+    $custom_config_machines = ['soolin.dice.priv', 'dice-vm-37-00.acrc.bris.ac.uk', 'vm-37-02.acrc.bris.ac.uk', 'vm03.phy.bris.ac.uk']
 
-    if member($custom_config_machines, $::fqdn){
-      file { '/etc/smartd.conf':
-        ensure  => 'present',
-        source => "puppet:///modules/${module_name}/smart.d/${fqdn}.conf",
+    if member($custom_config_machines, $facts['servername']) {
+      file { '/etc/smartmontools/smartd.conf':
+        source  => "puppet:///modules/${module_name}/smart.d/${facts['servername']}.conf",
         mode    => '0644',
         notify  => Service['smartd'],
-        require => [Package['smartmontools'],]
+        require => [Package['smartmontools'],],
       }
     }
     else {
-      file { '/etc/smartd.conf':
-        ensure  => 'present',
+      file { '/etc/smartmontools/smartd.conf':
         content => template("${module_name}/smartd.conf.erb"),
         mode    => '0644',
         notify  => Service['smartd'],
-        require => [Package['smartmontools'],]
+        require => [Package['smartmontools'],],
       }
     }
 
     service { 'smartd':
       ensure  => 'running',
       enable  => true,
-      require => [Package['smartmontools'],]
+      require => [Package['smartmontools'],],
     }
   }
 }
