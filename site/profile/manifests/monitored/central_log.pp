5df7c4dad8d5427cfb29f39b8418c8a151caadf8
Luke Kreczko <lkreczko@googlemail.com>
2022-08-24 16:36:09 +0100

    rsyslog: cleaning up recent changes

--- site/profile/manifests/monitored/central_log.pp
+++ site/profile/manifests/monitored/central_log.pp
@@ -1,38 +1,17 @@
 # manages the central logging behaviour of a machine (using rsyslog)
-
-# 220817 struggle to get syntax so jt-37-00 is true = central syslog
-# notify must be INSIDE the class
-#notify {"Running with \$fqdn ${::fqdn} defined":}
-
 class profile::monitored::central_log {
-notify {"Running with \$fqdn ${::fqdn} defined":}
   $central_log = lookup('profile::monitored::central_log', Array, 'first', ['*.* @10.129.5.3:514'])
   $it_services_log = lookup('profile::monitored::it_services_log', Array, 'first', [])
-# orig
-#  $is_central_log = member($central_log, $::fqdn) or member($central_log,
-#  $::ipaddress)
-# added :514 then ::514 - both fail
-#  $is_central_log = member($central_log, $::fqdn::514) or member($central_log,
-#  $::ipaddress::514)
-# Dr K says maybe syntax s/be
-#  $is_central_log = member($central_log, "^$::fqdn") or member($central_log,
-#  "^$::ipaddress")
-# doesn't work either
-#   $is_central_log = member($central_log, Regexp("^${::fqdn}")) or member($central_log,
-#   Regexp("^${::ipaddress}"))
 
-# try add :514
-#  $is_central_log = member($central_log, "^$::fqdn::514") or member($central_log,
-#  "^$::ipaddress::514")
-# DrK suggests other syntax: THAT WORKS
-   $is_central_log = member($central_log, "${::fqdn}:514") or member($central_log,
-   "${::ipaddress}:514")
+  $is_central_log = member($central_log, "${facts['networking']['fqdn']}:514") or member($central_log, "${facts['networking']['ip']}:514")
 
-
-notify {"Running with \$is_central_log $is_central_log defined":}
+  notify { 'rsyslog':
+    message => "Running on \$fqdn ${facts['networking']['fqdn']} with \$is_central_log ${is_central_log} defined",
+    level   => 'debug',
+  }
 
   unless $is_central_log {
-    file {'/etc/rsyslog.d':
+    file { '/etc/rsyslog.d':
       ensure => 'directory',
       owner  => 'root',
       group  => 'root',
@@ -40,17 +19,17 @@ notify {"Running with \$is_central_log $is_central_log defined":}
     }
     # remove obsolete configs
     file { [
-              '/etc/rsyslog.d/central.conf',
-              '/etc/rsyslog.d/00_docker.conf',
-              '/etc/rsyslog.d/10_local.conf',
-              '/etc/rsyslog.d/20_dice.conf',
-              '/etc/rsyslog.d/99_it_services.conf'
-          ]:
-      ensure => 'absent',
+        '/etc/rsyslog.d/central.conf',
+        '/etc/rsyslog.d/00_docker.conf',
+        '/etc/rsyslog.d/10_local.conf',
+        '/etc/rsyslog.d/20_dice.conf',
+        '/etc/rsyslog.d/99_it_services.conf',
+      ]:
+        ensure => 'absent',
     }
 
     file { '/etc/rsyslog.conf':
-      ensure  => 'present',
+      ensure  => 'file',
       content => template("${module_name}/etc/rsyslog.conf.erb"),
       mode    => '0644',
       notify  => Service['rsyslog'],
