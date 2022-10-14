962057a918ca6c7cdd22124e367d2a21b5a5b2c9
kreczko <lkreczko@googlemail.com>
2022-10-14 14:36:53 +0100

    fix: allow to ignore ganglia for newer machines

--- site/profile/manifests/monitored.pp
+++ site/profile/manifests/monitored.pp
@@ -1,7 +1,14 @@
 # just a simpele profile to summarise all monitoring
 class profile::monitored {
-  # ganglia
-  include ::profile::monitored::ganglia
+  $next_gen_monitoring = lookup('profile::monitored::next_gen_monitoring', Boolean, 'first', false)
+  unless $next_gen_monitoring {
+    # ganglia
+    include ::profile::monitored::ganglia
+  }
+
+  # next-gen monitoring
+  include ::profile::monitored::prometheus
+
   # smartd
   include ::profile::monitored::smartd
 
---
+++ site/profile/manifests/monitored/prometheus.pp
@@ -0,0 +1,6 @@
+# profile for prometheus monitoring
+class profile::monitored::prometheus {
+  notify { 'profile::monitored::prometheus':
+    message => 'work in progress',
+  }
+}
