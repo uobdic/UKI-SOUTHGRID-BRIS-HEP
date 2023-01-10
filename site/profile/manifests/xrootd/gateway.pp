ef36fd19938f943611f8baf67221dc633945b992
Luke Kreczko <lkreczko@googlemail.com>
2023-01-10 14:59:26 +0000

    fix: message --> notify

--- site/profile/manifests/xrootd/gateway.pp
+++ site/profile/manifests/xrootd/gateway.pp
@@ -1,4 +1,6 @@
 # profile for the xrootd manager (there can be more than one - can construct a hierarchy)
 class profile::xrootd::gateway {
-  message('xrootd::gateway placeholder')
+  notify { 'xrootd::gateway':
+    message => 'placeholder module',
+  }
 }
