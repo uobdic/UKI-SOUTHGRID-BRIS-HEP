3275518da3831dee0386bfbe8500536a99036e46
kreczko <lkreczko@googlemail.com>
2023-02-09 14:17:50 +0000

    profile: role and profiles for jupyterhub_and_dask_gateway

---
+++ site/profile/manifests/jupyterhub.pp
@@ -0,0 +1,5 @@
+# Sets up a JupyterHub server
+# https://jupyterhub.readthedocs.io/en/stable/installation-guide.html
+class profile::jupyterhub {
+  notice('Setting up JupyterHub - JUST A PLACEHOLDER FOR NOW')
+}
