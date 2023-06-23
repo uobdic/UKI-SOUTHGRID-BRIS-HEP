e0af15fd3d137b2bd2c44ff1782fe3be64baa7e2
Luke Kreczko <lkreczko@googlemail.com>
2023-06-23 09:26:19 +0100

    fix: has_key no longer supported in Puppet 7 -> removed

--- site/site/manifests/basic.pp
+++ site/site/manifests/basic.pp
@@ -17,8 +17,8 @@ class site::basic {
       '/etc/puppetlabs/facter/',
       '/etc/puppetlabs/facter/facts.d']: ensure => directory, }
 
-    if ($site_info and has_key($site_info, 'hepspec06_baseline')) {
-      if ($node_info and has_key($node_info, 'hepspec06')) {
+    if ($site_info and $site_info['hepspec06_baseline']) {
+      if ($node_info and $node_info['hepspec06']) {
         $baseline                = 0 + $site_info['hepspec06_baseline']
         $node_value              = 0 + $node_info['hepspec06']
         $accounting_scale_factor = $node_value / $baseline
