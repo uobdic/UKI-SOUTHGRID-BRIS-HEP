4e0ec364f9422adc2587e892e8e5ef9404ac41a9
Luke Kreczko <lkreczko@googlemail.com>
2023-06-27 15:15:24 +0100

    feat: replaced ganglia wit prometheus

--- site/profile/manifests/monitored.pp
+++ site/profile/manifests/monitored.pp
@@ -1,14 +1,7 @@
 # just a simpele profile to summarise all monitoring
 class profile::monitored {
-  $next_gen_monitoring = lookup('profile::monitored::next_gen_monitoring', Boolean, 'first', false)
-
-  # ganglia
-  include ::profile::monitored::ganglia
-
-  if $next_gen_monitoring {
-    # next-gen monitoring
-    include ::profile::monitored::prometheus
-  }
+  # default metric collection
+  include ::profile::monitored::prometheus
 
   # smartd
   include ::profile::monitored::smartd
