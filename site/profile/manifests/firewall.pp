ceb53fc88e83f33b310087b50d80f432fa26f0bf
kreczko <lkreczko@googlemail.com>
2023-08-25 10:23:22 +0100

    feat: add switch between iptables and firewalld

--- site/profile/manifests/firewall.pp
+++ site/profile/manifests/firewall.pp
@@ -2,33 +2,41 @@
 #  Firewall profile
 #
 class profile::firewall {
-  $accept          = lookup('profile::firewall::accept', Hash, 'deep', {})
-  $accept_v6       = lookup('profile::firewall6::accept', Hash, 'deep', {})
-  $drop            = lookup('profile::firewall::drop', Hash, 'deep', {})
-  $drop_v6         = lookup('profile::firewall6::drop', Hash, 'deep', {})
-  include profile::firewall::setup
+  $firewall_type   = lookup('node_info::firewall', String, 'deep', 'iptables')
 
-  $accept_defaults = {
-    'action' => 'accept',
-  }
-  $accept_defaults_v6 = {
-    'action' => 'accept',
-    'provider' => 'ip6tables',
-  }
+  # if firewall_type is iptables or does not start with firewalld, use iptables
+  # otherwise use firewalld
+  if $firewall_type == 'iptables' or ! $firewall_type =~ /^firewalld/ {
+    # use iptables
+    $accept          = lookup('profile::firewall::accept', Hash, 'deep', {})
+    $accept_v6       = lookup('profile::firewall6::accept', Hash, 'deep', {})
+    $drop            = lookup('profile::firewall::drop', Hash, 'deep', {})
+    $drop_v6         = lookup('profile::firewall6::drop', Hash, 'deep', {})
 
-  $drop_defaults   = {
-    'action' => 'drop',
-    'proto' => 'all',
-  }
-  $drop_defaults_v6   = {
-    'action' => 'drop',
-    'provider' => 'ip6tables',
-    'proto' => 'all',
-  }
+    $accept_defaults = {
+      'action' => 'accept',
+    }
+    $accept_defaults_v6 = {
+      'action' => 'accept',
+      'provider' => 'ip6tables',
+    }
 
-  create_resources('firewall', $accept, $accept_defaults)
-  create_resources('firewall', $drop, $drop_defaults)
+    $drop_defaults   = {
+      'action' => 'drop',
+      'proto' => 'all',
+    }
+    $drop_defaults_v6   = {
+      'action' => 'drop',
+      'provider' => 'ip6tables',
+      'proto' => 'all',
+    }
+    include profile::firewall::setup
+    create_resources('firewall', $accept, $accept_defaults)
+    create_resources('firewall', $drop, $drop_defaults)
 
-  create_resources('firewall', $accept_v6, $accept_defaults_v6)
-  create_resources('firewall', $drop_v6, $drop_defaults_v6)
+    create_resources('firewall', $accept_v6, $accept_defaults_v6)
+    create_resources('firewall', $drop_v6, $drop_defaults_v6)
+  } else {
+    include profile::firewalld::setup
+  }
 }
