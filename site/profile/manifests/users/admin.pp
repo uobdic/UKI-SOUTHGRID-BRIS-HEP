13a63f2bdea4091ab13b062ad5c27d9ca1344381
kreczko <lkreczko@googlemail.com>
2023-10-25 12:12:50 +0100

    add profile::users::admin

---
+++ site/profile/manifests/users/admin.pp
@@ -0,0 +1,21 @@
+# Profile for sudo users
+class profile::users::admin {
+  # purge sudo config and sudoers.d
+  class { 'sudo': }
+  # collect all users with sudo access
+  $site_admins = hiera_array('site::admins', [])
+  $node_admins = hiera_array('site::node_info::admins', [])
+  $sudoers = unique($site_admins + $node_admins)
+  # create sudoers config for each user
+  $sudoers.each |$user| {
+    sudo::conf { $user:
+      priority => 10,
+      content  => "${user} ALL=(ALL) NOPASSWD: ALL",
+    }
+  }
+  # ensure backwards compatibility for local-sudo:
+  sudo::conf { 'local-sudo':
+    priority => 10,
+    content  => '%local-sudo ALL=(ALL) NOPASSWD: ALL',
+  }
+}
