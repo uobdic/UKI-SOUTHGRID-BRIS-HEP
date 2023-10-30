8457b0096b259cecb3ee4cf1a49c8e2b9667913b
kreczko <lkreczko@googlemail.com>
2023-10-30 10:55:08 +0000

    fix: simplify if-else with get-function and default

--- site/profile/manifests/users/admin.pp
+++ site/profile/manifests/users/admin.pp
@@ -3,17 +3,12 @@ class profile::users::admin {
   # purge sudo config and sudoers.d
   class { 'sudo': }
   # collect all users with sudo access
-  Array[String] $site_admins = hiera_array('site::admins', [])
+  $site_admins = hiera_array('site::admins', [])
   if $facts['node_info']['admins'] == undef {
     fail('node_info.admins is not defined')
   }
-  if has_key($facts['node_info'], 'admins') {
-    Array[String] $node_admins = $facts['node_info']['admins']
-  }
-  else {
-    Array[String] $node_admins = []
-  }
-  Array[String] $sudoers = unique($site_admins + $node_admins)
+  $node_admins = get($facts['node_info'], 'admins', [])
+  $sudoers = unique($site_admins + $node_admins)
   # create sudoers config for each user
   $sudoers.each |$user| {
     sudo::conf { $user:
