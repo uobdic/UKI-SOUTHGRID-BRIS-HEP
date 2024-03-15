57dceddac87564aabe04bc5b3c6751652efca8ab
kreczko <lkreczko@googlemail.com>
2024-03-15 12:40:15 +0000

    monitored: remove prometheus from default monitoring

--- site/profile/manifests/monitored.pp
+++ site/profile/manifests/monitored.pp
@@ -1,14 +1,11 @@
 # just a simpele profile to summarise all monitoring
 class profile::monitored {
-  # default metric collection
-  include ::profile::monitored::prometheus
-
   # smartd
-  include ::profile::monitored::smartd
+  include profile::monitored::smartd
 
   # central log
-  include ::profile::monitored::central_log
+  include profile::monitored::central_log
 
   # psacct
-  include ::profile::monitored::psacct
+  include profile::monitored::psacct
 }
