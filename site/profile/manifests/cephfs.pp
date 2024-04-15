7f7e0ae8cab2d00b7ab15982f2150f0b1a81ccd2
kreczko <lkreczko@googlemail.com>
2024-04-15 13:26:13 +0100

    cephfs: add keyrings to config

--- site/profile/manifests/cephfs.pp
+++ site/profile/manifests/cephfs.pp
@@ -3,20 +3,36 @@
 # @param install_ceph - whether to install the ceph client or not
 # @param mounts - a hash of mounts to create; takes normal mount options
 class profile::cephfs (
-  Array[String] $cephfs_keys = ['dice-reader'],
-  Boolean $install_ceph = false,
+  Array[String] $keys = ['dice-reader'],
   Hash $mounts = {},
 ) {
-  if $install_ceph {
-    yumrepo { 'ceph-reef':
-      baseurl  => 'https://download.ceph.com/rpm-reef/el9/x86_64',
-      descr    => 'Ceph Reef',
-      enabled  => 1,
-      gpgcheck => 0,
+  yumrepo { 'ceph-reef':
+    baseurl  => 'https://download.ceph.com/rpm-reef/el9/x86_64',
+    descr    => 'Ceph Reef',
+    enabled  => 1,
+    gpgcheck => 0,
+  }
+  package { 'ceph':
+    ensure => latest,
+  }
+
+  # create the cephfs keys
+  $keys.each |$key| {
+    file { "/etc/ceph/ceph.client.${key}.keyring":
+      ensure => file,
+      source => "puppet:///dice_storage/cephfs/ceph.client.${key}.keyring",
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0600',
     }
-    package { 'ceph':
-      ensure => latest,
-    }
+  }
+
+  file { '/etc/ceph/ceph.conf':
+    ensure => file,
+    source => 'puppet:///dice_storage/cephfs/ceph.conf',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
   }
 
   # create the mounts
