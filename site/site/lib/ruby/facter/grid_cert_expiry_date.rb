53bc8ed21bcf67f2c5e13c43a9e31b58633568b6
kreczko <lkreczko@googlemail.com>
2024-04-23 16:15:55 +0100

    add grid_cert_expiry_date fact

---
+++ site/site/lib/ruby/facter/grid_cert_expiry_date.rb
@@ -0,0 +1,10 @@
+Facter.add('grid_cert_expiry_date') do
+    setcode do
+        cert_path = '/etc/grid-security/hostcert.pem'
+        if File.exist?(cert_path)
+        cert = OpenSSL::X509::Certificate.new(File.read(cert_path))
+        expiry_date = cert.not_after
+        expiry_date.strftime('%Y-%m-%d')
+        end
+    end
+    end
