7da01f6679b5646daafbbc9aa6c634d632a59dd7
kreczko <lkreczko@googlemail.com>
2024-04-23 13:31:20 +0100

    profile(service_accounts): manage $home

--- site/profile/manifests/service_accounts.pp
+++ site/profile/manifests/service_accounts.pp
@@ -20,4 +20,13 @@ class profile::service_accounts (
 
   create_resources('group', $groups, $group_defaults)
   create_resources('accounts::user', $users, $acc_defaults)
+  $users.each |$key, $value| {
+    file { $value['home']:
+      ensure  => directory,
+      owner   => $key,
+      group   => $value['group'],
+      mode    => '0700',
+      require => User[$key],
+    }
+  }
 }
