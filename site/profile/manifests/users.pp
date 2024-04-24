551046bb595c5348c6482140d90f2a8aa455f4b5
kreczko <lkreczko@googlemail.com>
2024-04-24 09:28:44 +0100

    profile(users): no symlinks on sts

--- site/profile/manifests/users.pp
+++ site/profile/manifests/users.pp
@@ -50,17 +50,19 @@ class profile::users {
       }
     }
     # symlink to the correct location (/home -> /users) for each user
-    $users.each |$key, $value| {
-      unless $value['ensure'] == 'absent' or "/home/${key}" == $value['home'] {
-        file { "/home/${key}":
-          ensure  => link,
-          target  => $value['home'],
-          require => User[$key],
+    unless $fqdn == 'sts.dice.priv' {
+      $users.each |$key, $value| {
+        unless $value['ensure'] == 'absent' or "/home/${key}" == $value['home'] {
+          file { "/home/${key}":
+            ensure  => link,
+            target  => $value['home'],
+            require => User[$key],
+          }
         }
-      }
-      if $value['ensure'] == 'absent' and "/home/${key}" != $value['home'] {
-        file { "/home/${key}":
-          ensure => absent,
+        if $value['ensure'] == 'absent' and "/home/${key}" != $value['home'] {
+          file { "/home/${key}":
+            ensure => absent,
+          }
         }
       }
     }
