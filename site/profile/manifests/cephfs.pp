f9858e55c7b4f926c2b6c23bdc7ca23e6271a545
kreczko <lkreczko@googlemail.com>
2024-04-25 12:37:36 +0100

    profile::cephfs: add CentOS7 mounts

--- site/profile/manifests/cephfs.pp
+++ site/profile/manifests/cephfs.pp
@@ -5,10 +5,20 @@ class profile::cephfs (
   Array[String] $keys = ['dice-reader'],
   Hash $mounts = {},
 ) {
-  package { 'centos-release-ceph-reef': }
-  package { 'ceph-common':
-    ensure  => latest,
-    require => [Package['centos-release-ceph-reef']],
+  $ceph_release = $facts['os']['release']['major'] ? {
+    '7' => 'https://download.ceph.com/rpm-octopus/el7/noarch/ceph-release-1-1.el7.noarch.rpm',
+    default => 'centos-release-ceph-reef',
+  }
+  $ceph_mount_dependency = $facts['os']['release']['major'] ? {
+    '7' => 'ceph-fuse',
+    default => 'ceph-common',
+  }
+
+  package { $ceph_release: }
+  package { $ceph_mount_dependency:
+    ensure     => latest,
+    require    => [Package[$ceph_release]],
+    enablerepo => 'epel',
   }
 
   # create the cephfs keys
@@ -47,15 +57,34 @@ class profile::cephfs (
       file { "/cephfs/${mount_location}":
         ensure  => link,
         target  => $mount_location,
-        require => File[$mount_location],
+        require => [File[$mount_location], Mount[$mount_location]],
       }
     }
-    $defaults = {
-      'require'  => [File['/etc/ceph/ceph.conf'], File[$mount_locations]],
-      'fstype'   => 'ceph',
-      'ensure'   => 'mounted',
-      'options'  => 'noatime,_netdev',
+    if $facts['os']['release']['major'] == '9' {
+      $defaults = {
+        'require'  => [File['/etc/ceph/ceph.conf'], File[$mount_locations]],
+        'fstype'   => 'ceph',
+        'ensure'   => 'mounted',
+        'options'  => 'noatime,_netdev',
+      }
+      create_resources('mount', $mounts, $defaults)
+    } else {
+      $ceph_fuse_mounts = $mounts.map |$mount, $options| {
+        # default options are of the form "dice-user@.dicefs=/dice"
+        # we want to extract the client ID before the '@' sign
+        $client_id = $options['device'].split('@')[0]
+        $ceph_fuse_mount = {
+          $mount => {
+            'ensure'  => 'mounted',
+            'device'  => 'none',
+            'fstype'  => 'ceph',
+            'options' => "ceph.id=${client_id},ceph.client_mountpoint=${mount},noatime,_netdev",
+            'require' => [File['/etc/ceph/ceph.conf'], File[$mount]],
+          },
+        }
+        $ceph_fuse_mount
+      }
+      create_resources('mount', $ceph_fuse_mounts)
     }
-    create_resources('mount', $mounts, $defaults)
   }
 }
