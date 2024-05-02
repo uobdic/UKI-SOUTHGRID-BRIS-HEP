34784fa4440e0fdd32043fd44caf6eed90672fd5
kreczko <lkreczko@googlemail.com>
2024-05-02 10:19:46 +0100

    add squid role and profile

---
+++ site/profile/manifests/squid.pp
@@ -0,0 +1,19 @@
+# Configure Squid proxy server (https://twiki.cern.ch/twiki/bin/view/Frontier/InstallSquid)
+class profile::squid {
+  package { 'frontier-squid-release':
+    ensure   => 'present',
+    provider => 'yum',
+    source   => 'http://frontier.cern.ch/dist/rpms/RPMS/noarch/frontier-release-1.2-1.noarch.rpm',
+  }
+
+  package { 'frontier-squid':
+    ensure  => 'present',
+    require => Package['frontier-squid-release'],
+  }
+
+  service { 'frontier-squid':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['frontier-squid'],
+  }
+}
