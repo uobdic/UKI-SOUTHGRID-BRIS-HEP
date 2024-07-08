07ba29852f4a96fc4074458f7a2aa4ed5df3a928
kreczko <lkreczko@googlemail.com>
2024-07-08 16:28:10 +0100

    fix settings for apptainer

--- site/profile/manifests/heposlibs.pp
+++ site/profile/manifests/heposlibs.pp
@@ -1,7 +1,7 @@
 # Class for installing HEP_OSlibs
 class profile::heposlibs {
-  String $repo_rpm = 'https://linuxsoft.cern.ch/wlcg/el9/x86_64/wlcg-repo-1.0.0-1.el9.noarch.rpm'
-  String $package_name = 'HEP_OSlibs'
+  $repo_rpm = 'https://linuxsoft.cern.ch/wlcg/el9/x86_64/wlcg-repo-1.0.0-1.el9.noarch.rpm'
+  $package_name = 'HEP_OSlibs'
 
   package { 'wlcg-repo':
     ensure   => present,
