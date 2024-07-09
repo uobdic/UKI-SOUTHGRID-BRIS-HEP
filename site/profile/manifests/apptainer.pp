50f986275cbf937619afc25846a45ea2e1a59154
kreczko <lkreczko@googlemail.com>
2024-07-09 15:14:55 +0100

    profile::apptainer: also add condor config if included on a condor execute node

--- site/profile/manifests/apptainer.pp
+++ site/profile/manifests/apptainer.pp
@@ -1,4 +1,19 @@
 # class for apptainer profile
 class profile::apptainer {
   include apptainer
+  # if on a condor execute node, also add config:
+  $condor_role = lookup('profile::htcondor::role')
+  if $condor_role == 'execute' {
+    $bind_paths = lookup('apptainer::bind_paths')
+    file {'/etc/condor/config.d/50-container.conf':
+    ensure  => file,
+    content => template('profile/etc/condor/50-container.conf.erb'),
+  }
+  file {'/etc/condor/container_wrapper':
+    ensure => file,
+    source => "puppet:///modules/${module_name}/etc/condor/container_wrapper",
+    mode   => '0755',
+  }
+  }
+
 }
