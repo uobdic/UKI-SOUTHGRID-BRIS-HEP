e3dc2e0debe2700f9bafb208009368869ca92e1a
kreczko <lkreczko@googlemail.com>
2025-02-25 10:59:04 +0000

    htcondor: add get_bind_mounts script

--- site/profile/manifests/apptainer.pp
+++ site/profile/manifests/apptainer.pp
@@ -5,15 +5,19 @@ class profile::apptainer {
   $condor_role = lookup('profile::htcondor::role')
   if $condor_role == 'execute' {
     $bind_paths = lookup('apptainer::bind_paths')
-    file {'/etc/condor/config.d/50-container.conf':
-    ensure  => file,
-    content => template('profile/etc/condor/50-container.conf.erb'),
+    file { '/etc/condor/config.d/50-container.conf':
+      ensure  => file,
+      content => template('profile/etc/condor/50-container.conf.erb'),
+    }
+    file { '/etc/condor/container_wrapper':
+      ensure => file,
+      source => "puppet:///modules/${module_name}/etc/condor/container_wrapper",
+      mode   => '0755',
+    }
+    file { '/etc/condor/get_bind_mounts':
+      ensure => file,
+      source => "puppet:///modules/${module_name}/etc/condor/get_bind_mounts",
+      mode   => '0755',
+    }
   }
-  file {'/etc/condor/container_wrapper':
-    ensure => file,
-    source => "puppet:///modules/${module_name}/etc/condor/container_wrapper",
-    mode   => '0755',
-  }
-  }
-
 }
