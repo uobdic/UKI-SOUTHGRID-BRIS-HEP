96ec67513133053099e5afc9098f07ed8bc7bee4
Luke Kreczko <lkreczko@gmail.com>
2025-03-12 14:31:37 +0100

    do not run profile::firewalld and ::login on EL7 nodes

--- site/profile/manifests/login.pp
+++ site/profile/manifests/login.pp
@@ -9,32 +9,34 @@ class profile::login (
   String $krb5_realm,
   String $krb5_kpasswd,
 ) {
-  sssd::domain { 'dice.priv':
-    id_provider       => 'files',
-    auth_provider     => 'krb5',
-    chpass_provider   => 'krb5',
-    access_provider   => 'simple',
-    cache_credentials => true,
-    enumerate         => true,
-    debug_level       => 9,
-  }
+  unless $facts['os']['release']['major'] < 8 {
+    sssd::domain { 'dice.priv':
+      id_provider       => 'files',
+      auth_provider     => 'krb5',
+      chpass_provider   => 'krb5',
+      access_provider   => 'simple',
+      cache_credentials => true,
+      enumerate         => true,
+      debug_level       => 9,
+    }
 
-  sssd::provider::krb5 { 'dice.priv':
-    krb5_server  => $krb5_server,
-    krb5_realm   => $krb5_realm,
-    krb5_kpasswd => $krb5_kpasswd,
-  }
+    sssd::provider::krb5 { 'dice.priv':
+      krb5_server  => $krb5_server,
+      krb5_realm   => $krb5_realm,
+      krb5_kpasswd => $krb5_kpasswd,
+    }
 
-  # execute authselect, if not already set to sssd
-  exec { 'authselect':
-    command => 'authselect select sssd --force',
-    unless  => 'authselect current | grep -q sssd',
-    path    => ['/usr/sbin', '/usr/bin'],
-  }
-  # now krb5:
-  file { '/etc/krb5.conf':
-    ensure => file,
-    source => 'puppet:///dice_store/krb5/krb5.conf', # lint:ignore:puppet_url_without_modules
-    notify => Service['sssd'],
+    # execute authselect, if not already set to sssd
+    exec { 'authselect':
+      command => 'authselect select sssd --force',
+      unless  => 'authselect current | grep -q sssd',
+      path    => ['/usr/sbin', '/usr/bin'],
+    }
+    # now krb5:
+    file { '/etc/krb5.conf':
+      ensure => file,
+      source => 'puppet:///dice_store/krb5/krb5.conf', # lint:ignore:puppet_url_without_modules
+      notify => Service['sssd'],
+    }
   }
 }
