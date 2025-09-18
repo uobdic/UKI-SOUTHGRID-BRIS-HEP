4b5dbf7c7dbb19f93649c0190ae4f05f6f7076ec
kreczko <lkreczko@googlemail.com>
2025-09-18 13:50:22 +0100

    fix(profile::apptainer): switch from deprecated to recommended lookup behaviour

--- site/profile/manifests/apptainer.pp
+++ site/profile/manifests/apptainer.pp
@@ -2,7 +2,7 @@
 class profile::apptainer {
   include apptainer
   # if on a condor execute node, also add config:
-  $condor_role = lookup('profile::htcondor::role', 'none')
+  $condor_role = lookup('profile::htcondor::role', undef, undef, 'none')
   if $condor_role == 'execute' {
     $bind_paths = lookup('apptainer::bind_paths')
     file { '/etc/condor/config.d/50-container.conf':
