2ca5c4f385b08a356080d781b5dca3b53ed6810a
kreczko <lkreczko@googlemail.com>
2025-09-18 13:44:54 +0100

    fix(profile::apptainer): condor role lookup for non-condor hosts

--- site/profile/manifests/apptainer.pp
+++ site/profile/manifests/apptainer.pp
@@ -2,7 +2,7 @@
 class profile::apptainer {
   include apptainer
   # if on a condor execute node, also add config:
-  $condor_role = lookup('profile::htcondor::role')
+  $condor_role = lookup('profile::htcondor::role', 'none')
   if $condor_role == 'execute' {
     $bind_paths = lookup('apptainer::bind_paths')
     file { '/etc/condor/config.d/50-container.conf':
